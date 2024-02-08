0a504b38e3c53565af34ec3cbdbdcdfe08a919be
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-02-08 21:38:25 +0200

    Add new images and update assets paths, add dependencies for SVG and Google Fonts (Loging Page)

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,16 +1,17 @@
 import 'package:go_router/go_router.dart';
+import 'package:sls_mvp_microsoft/features/authentication/login_view.dart';
 import 'package:sls_mvp_microsoft/features/home/home_view.dart';
 
 abstract class AppRouter {
   // static const kHomeView = '/homeView';
   static final router = GoRouter(
     routes: [
-      // GoRoute(
-      //   path: '/',
-      //   builder: (context, state) => const SplashView(),
-      // ),
       GoRoute(
         path: '/',
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: '/home',
         builder: (context, state) => const HomeView(),
       ),
     ],
--- lib/features/authentication/widgets/login_view_body.dart
+++ lib/features/authentication/widgets/login_view_body.dart
@@ -1,10 +1,175 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sls_mvp_microsoft/constants.dart';
+import 'package:sls_mvp_microsoft/core/utils/assets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+final _emailController = TextEditingController();
+final _passwordController = TextEditingController();
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                AssetsData.logo,
+                height: 150,
+                color: kPrimaryColor,
+              ),
+
+              SizedBox(height: 25),
+
+              //welcome message
+              Text(
+                "Welcome Back!",
+                style: GoogleFonts.lobster(
+                    fontSize: 40, textStyle: TextStyle(color: kSecondColor)),
+              ),
+              SizedBox(height: 10),
+              //message
+
+              const Text(
+                "PEACE OF MIND, ON EVERY ROUTE. ✨",
+                style: const TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    color: kSecondColor),
+              ),
+              SizedBox(height: 50),
+
+              //Email Text Box
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: kSecondColor),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: 'Email'),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+
+              //password
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: kSecondColor),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: 'Password'),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.00),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) {
+                        //       return ForgotPasswordPage();
+                        //     },
+                        //   ),
+                        // );
+                      },
+                      child: Text("forgot password?",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 15),
+
+              //Sign in Button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: InkWell(
+                  //GestureDetector to InkWell for animation
+                  onTap: () {},
+
+                  child: Ink(
+                    //Container to Ink for animation
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        borderRadius: BorderRadius.circular(12)),
+                    child: Center(
+                      child: Text('Sign in',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18)),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25),
+
+              //not a  member button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) {
+                      //       return RegisterPage();
+                      //     },
+                      //   ),
+                      // );
+                    },
+                    child: GestureDetector(
+                      /*onTap: ,*/
+                      child: Text(' Register Now',
+                          style: TextStyle(
+                              color: Colors.blue, fontWeight: FontWeight.bold)),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 75),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
