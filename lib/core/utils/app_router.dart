272b7be200230225c5ae41875105a53e35e89012
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-02-09 01:25:16 +0200

    Add route for login view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,13 +2,13 @@ import 'package:go_router/go_router.dart';
 import 'package:sls_mvp_microsoft/features/authentication/login_view.dart';
 import 'package:sls_mvp_microsoft/features/authentication/register_view.dart';
 import 'package:sls_mvp_microsoft/features/home/home_view.dart';
-import 'package:sls_mvp_microsoft/features/home/widgets/home_view_body.dart';
 
 abstract class AppRouter {
   // static const kHomeView = '/homeView';
   static final router = GoRouter(
     routes: [
       GoRoute(
+        //hiiiiiiiiii
         path: '/',
         builder: (context, state) => const LoginView(),
       ),
