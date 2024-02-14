ab16474ef3cf9eb944ce61acc390569f96ce79b5
ahhassan88 <amhasan47896@gmail.com>
2024-02-14 15:52:25 +0200

    adding signout to ui

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -1,10 +1,15 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:sls_mvp_microsoft/constants.dart';
 import 'package:sls_mvp_microsoft/core/utils/assets.dart';
-import 'package:sls_mvp_microsoft/features/home/widgets/chat_view_body.dart';
-import 'package:sls_mvp_microsoft/features/home/widgets/home_view_body.dart';
-import 'package:sls_mvp_microsoft/features/home/widgets/settings_view_body.dart';
+import 'package:sls_mvp_microsoft/core/widgets/custom_loading_indicator.dart';
+import 'package:sls_mvp_microsoft/core/widgets/custom_snack_bar.dart';
+import 'package:sls_mvp_microsoft/features/home/view/widgets/chat_view_body.dart';
+import 'package:sls_mvp_microsoft/features/home/view/widgets/home_view_body.dart';
+import 'package:sls_mvp_microsoft/features/home/view/widgets/settings_view_body.dart';
+import 'package:sls_mvp_microsoft/features/home/view_model/signout_cubit/signout_cubit.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -24,33 +29,49 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                spreadRadius: 1,
-                blurRadius: 2,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                ),
+              ],
+            ),
+            child: AppBar(
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      BlocProvider.of<SignoutCubit>(context).signout();
+                    },
+                    icon: const Icon(Icons.logout))
+              ],
+              backgroundColor: kBGColor,
+              title: const Image(
+                height: 150,
+                image: AssetImage(AssetsData.logoMini),
               ),
-            ],
-          ),
-          child: AppBar(
-            backgroundColor: kBGColor,
-            title: Image(
-              height: 150,
-              image: AssetImage(AssetsData.logoMini),
             ),
           ),
         ),
-      ),
-      backgroundColor: kBGColor,
-      bottomNavigationBar: curvedBottomNav(),
-      body: SafeArea(
-        child: _pages[_selectedIndex], // Display current page based on index
-      ),
-    );
+        backgroundColor: kBGColor,
+        bottomNavigationBar: curvedBottomNav(),
+        body: BlocListener<SignoutCubit, SignoutState>(
+          listener: (context, state) {
+            if (state is SignoutSuccess) {
+              GoRouter.of(context).push('/login');
+            } else if (state is SignoutFailure) {
+              showSnackBar(context, "signout failure");
+            } else if (state is SignoutLoading) {}
+          },
+          child: SafeArea(
+            child:
+                _pages[_selectedIndex], // Display current page based on index
+          ),
+        ));
   }
 
   CurvedNavigationBar curvedBottomNav() {
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:sls_mvp_microsoft/constants.dart';
 import 'package:sls_mvp_microsoft/core/utils/app_router.dart';
+import 'package:sls_mvp_microsoft/features/home/view_model/signout_cubit/signout_cubit.dart';
 
 void main() {
   // Gemini.init(apiKey: kGeminiApi, enableDebugging: true);
@@ -19,13 +21,16 @@ class SLS extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: kBGColor,
-        // textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return BlocProvider(
+      create: (context) => SignoutCubit(),
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: kBGColor,
+          // textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
