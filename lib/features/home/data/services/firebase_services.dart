b9ac1a7700c3525a27ad0f1c3ab2f49ee9729fad
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-04-16 16:49:12 +0200

    Refactor FirebaseServices class and add vehicle upload functionality

--- lib/features/home/data/services/firebase_services.dart
+++ lib/features/home/data/services/firebase_services.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -9,30 +6,39 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FirebaseFirestoreServiceVehicles {
-  CollectionReference vehicles = FirebaseFirestore.instance.collection('vehicles');
+  CollectionReference vehicles =
+      FirebaseFirestore.instance.collection('vehicles');
 
   Future<void> addVehicle(
       {required String vehicleid,
-        required String vehicletype,
+      required String vehicletype,
       required String vehichlemodel,
       required String vehiclecolor,
       required String vehiclefueltype,
       required num vehiclefuelcapciaty,
-       required String vehiclemodel3durlpath}) {
-            final vehicleRef = vehicles.doc(vehicleid);
-        
-    return vehicleRef
-        .set({
-          'vehicle_type': vehicletype,
-          'vehicle_model': vehichlemodel,
-          'vehicle_color': vehiclecolor,
-          'vehicle_fuel_type': vehiclefueltype,
-          'vehicle_fuel_capacity': vehiclefuelcapciaty,
-                    // 'model3d_url': vehiclemodel3durl
+      required String vehiclemodel3durlpath}) {
+    //! check if vehicle already exists in realtime database
+    /*
+      if code readed from text box is in realtime database then 
+        if password read from text box is equal to password in realtime database then
+          add vehicle to firestore (code blow)
+          add vehicle code to firestore in user (Manger) as vehicle collection  
+        else
+          show error message
+     */
 
-        })
-        .then((value) async { print("Vehicle Added");
-                  FirebaseStorage storage = FirebaseStorage.instance;
+    final vehicleRef = vehicles.doc(vehicleid);
+
+    return vehicleRef.set({
+      'vehicle_type': vehicletype,
+      'vehicle_model': vehichlemodel,
+      'vehicle_color': vehiclecolor,
+      'vehicle_fuel_type': vehiclefueltype,
+      'vehicle_fuel_capacity': vehiclefuelcapciaty,
+      // 'model3d_url': vehiclemodel3durl
+    }).then((value) async {
+      print("Vehicle Added");
+      FirebaseStorage storage = FirebaseStorage.instance;
       Reference storageReference =
           storage.ref().child('vehicle_models_3d/$vehicleid.glb');
 // print(storageReference);
@@ -44,44 +50,41 @@ class FirebaseFirestoreServiceVehicles {
 // print(appDocPath);
 // print(appdoc);
 // String filePath = '${appDocDir.path}/$vehiclemodel3durlpath';
-// File file = File(filePath);   
-    
- if (!await File(vehiclemodel3durlpath).exists()) {
+// File file = File(filePath);
 
-          print("it doesn't exist");
-      throw Exception('The specified file does not exist.');
-    }else{
-            UploadTask uploadTask = storageReference.putFile(File(vehiclemodel3durlpath));
-print(uploadTask);
-      await uploadTask.whenComplete(() async {
-        String downloadUrl = await storageReference.getDownloadURL();
+      if (!await File(vehiclemodel3durlpath).exists()) {
+        print("it doesn't exist");
+        throw Exception('The specified file does not exist.');
+      } else {
+        UploadTask uploadTask =
+            storageReference.putFile(File(vehiclemodel3durlpath));
+        print(uploadTask);
+        await uploadTask.whenComplete(() async {
+          String downloadUrl = await storageReference.getDownloadURL();
 
-        await vehicles
-            .doc(vehicleid)
-            .update({'vehicle_3d_model_url': downloadUrl});
+          await vehicles
+              .doc(vehicleid)
+              .update({'vehicle_3d_model_url': downloadUrl});
+        });
+      }
+
+      //! don't add vehicle to realtime database
+      final database = FirebaseDatabase.instance.ref('/car');
+      final userRef = database.child(vehicleid);
+      await userRef.set({
+        "battery": 40,
+        "door": true,
+        "fatis": "R",
+        "fuel": 20,
+        "ignition": false,
+        "manualbrake": true,
+        "rpm": 4.8,
+        "sleep": false,
+        "speed": 30.5,
+        "steering": 30,
+        "temp": 0,
+        "timestamp": "02/01/2024 16:22"
       });
-    }
-  final database = FirebaseDatabase.instance.ref('/car');
-  final userRef = database.child(vehicleid);
-  await userRef.set({
-     "battery": 40,
-    "door": true,
-    "fatis": "R",
-    "fuel": 20,
-    "ignition": false,
-    "manualbrake": true,
-    "rpm": 4.8,
-    "sleep": false,
-    "speed": 30.5,
-    "steering": 30,
-    "temp": 0,
-    "timestamp": "02/01/2024 16:22"
-  });
-
-
-
-
-        })
-        .catchError((error) => print("Failed to add Vehicle: $error"));
+    }).catchError((error) => print("Failed to add Vehicle: $error"));
   }
 }
--- lib/features/home/view_model/vehicles/vehicles_cubit.dart
+++ lib/features/home/view_model/vehicles/vehicles_cubit.dart
@@ -13,135 +13,123 @@ part 'vehicles_state.dart';
 class VehiclesCubit extends Cubit<VehiclesState> {
   VehiclesCubit() : super(VehiclesInitial());
 
-  Future<void> addVehicle (
-     {required String vehicleid,
+  Future<void> addVehicle(
+      {required String vehicleid,
       required String vehicletype,
       required String vehichlemodel,
       required String vehiclecolor,
       required String vehiclefueltype,
       required num vehiclefuelcapciaty,
-       required String vehiclemodel3durlpath}
-  )async{
-
-
+      required String vehiclemodel3durlpath}) async {
     // // Handle platform-specific paths
     // final absolutePath = Platform.isAndroid
     //     ? vehiclemodel3durlpath.path
     //     : File(vehiclemodel3durlpath.path).absolutePath;
     emit(VehiclesAddLoading());
-try {
-
-  await FirebaseFirestoreServiceVehicles().addVehicle(vehicleid:vehicleid ,vehicletype: vehicletype,vehiclecolor: vehiclecolor,vehichlemodel:vehichlemodel,vehiclefuelcapciaty: vehiclefuelcapciaty,vehiclefueltype: vehiclefueltype,vehiclemodel3durlpath: vehiclemodel3durlpath);
-emit(VehiclesAddSuccess());
-}catch (e) { 
-emit(VehiclesAddFailure());
-}
-  
+    try {
+      await FirebaseFirestoreServiceVehicles().addVehicle(
+          vehicleid: vehicleid,
+          vehicletype: vehicletype,
+          vehiclecolor: vehiclecolor,
+          vehichlemodel: vehichlemodel,
+          vehiclefuelcapciaty: vehiclefuelcapciaty,
+          vehiclefueltype: vehiclefueltype,
+          vehiclemodel3durlpath: vehiclemodel3durlpath);
+      emit(VehiclesAddSuccess());
+    } catch (e) {
+      emit(VehiclesAddFailure());
+    }
   }
 
+  late List<Map<String, dynamic>> carParsed = [];
+  late List<Map<String, dynamic>> carParsedrealtime = [];
 
-    late List<Map<String,dynamic>> carParsed = [];
-        late List<Map<String,dynamic>> carParsedrealtime=[];
-
-  Future<void> getVehicle () async{
-
-                // final database =   FirebaseDatabase.instance.ref('/car');
+  Future<void> getVehicle() async {
+    // final database =   FirebaseDatabase.instance.ref('/car');
 
 // final ds = await database.once(DatabaseEventType.value);
 //           String s = json.encode(ds.snapshot.value);
 
-
-print('entered gettijg');
+    print('entered gettijg');
 
     emit(VehichlesGetLoading());
-try {
-
-  print('entered try getting');
-final QuerySnapshot querySnapshot= await FirebaseFirestore.instance
-    .collection('vehicles') 
-    .get();
+    try {
+      print('entered try getting');
+      final QuerySnapshot querySnapshot =
+          await FirebaseFirestore.instance.collection('vehicles').get();
       print('enterere for loop');
       print('this car parsed before even starting $carParsed');
       carParsed.clear();
-        for (var doc in querySnapshot.docs) {
-          // final childbase = database.child('${doc.id}');
-          // final event = await childbase.once(DatabaseEventType.value);
+      for (var doc in querySnapshot.docs) {
+        // final childbase = database.child('${doc.id}');
+        // final event = await childbase.once(DatabaseEventType.value);
 
-          print('elements');
-          // print(doc.data());
-          String test = json.encode(doc.data());
-          // print(test);
-          Map<String,dynamic> decoded = json.decode(test);
-          print(doc.id);
-          decoded['id']=doc.id;
-          print(doc.data());
-          print(decoded['id']);
-          print(decoded);
-         carParsed.add(decoded);
-         print('this is car parsed car pasrsed car parsed$carParsed');
+        print('elements');
+        // print(doc.data());
+        String test = json.encode(doc.data());
+        // print(test);
+        Map<String, dynamic> decoded = json.decode(test);
+        print(doc.id);
+        decoded['id'] = doc.id;
+        print(doc.data());
+        print(decoded['id']);
+        print(decoded);
+        carParsed.add(decoded);
+        print('this is car parsed car pasrsed car parsed$carParsed');
         //  print(carParsedrealtime);
-print('element added');
-        }
-        print('outside loop after car parsed ended $carParsed');
-        //   final event = await database.once(DatabaseEventType.value);
-        //   for(var childsnapshot in event.snapshot.children){
-        //               print('realtime entered');
+        print('element added');
+      }
+      print('outside loop after car parsed ended $carParsed');
+      //   final event = await database.once(DatabaseEventType.value);
+      //   for(var childsnapshot in event.snapshot.children){
+      //               print('realtime entered');
 
-        //   String test2 = json.encode(childsnapshot.value);
-        //             print("this is the test ${test2}");
-        //   Map<String,dynamic> decoded2 = json.decode(test2);
-        //  carParsedrealtime.add(decoded2);
+      //   String test2 = json.encode(childsnapshot.value);
+      //             print("this is the test ${test2}");
+      //   Map<String,dynamic> decoded2 = json.decode(test2);
+      //  carParsedrealtime.add(decoded2);
 
- 
-        //   }
-  //       final ds = await database.once(DatabaseEventType.value);
-  //       print(ds.toString());
-  // carParsedrealtime= json.decode(json.encode(ds.snapshot.value)).toList();
+      //   }
+      //       final ds = await database.once(DatabaseEventType.value);
+      //       print(ds.toString());
+      // carParsedrealtime= json.decode(json.encode(ds.snapshot.value)).toList();
 // print(carParsedrealtime);
-print('outisdfffffffffffffffffffffffffff');
+      print('outisdfffffffffffffffffffffffffff');
 //                 carRaw  =  json.encode(vehicles[0]);
 //                 print(carRaw);
 //                         print('car raw');
 // carParsed = json.decode(carRaw);
-        print('car parsedddddddddddddddddddddddddddddddddddddddddddddddddddd');
+      print('car parsedddddddddddddddddddddddddddddddddddddddddddddddddddd');
 //         // print(carRaw);
 //             print(carParsed);
 // print(carParsed);
-emit(VehiclesGetSuccess());
-}catch (e) { 
-emit(VehiclesGetFailure());
-return;
-}
-
-
-
-
+      emit(VehiclesGetSuccess());
+    } catch (e) {
+      emit(VehiclesGetFailure());
+      return;
+    }
   }
 
-  
-Future<void> getVehiclerealtime () async{
+  Future<void> getVehiclerealtime() async {
+//! if viechle name is in firestore inside user add it to the list
+    emit(VehichlesGetLoading());
+    final database = FirebaseDatabase.instance.ref('/car');
 
-  emit(VehichlesGetLoading());
-                final database =   FirebaseDatabase.instance.ref('/car');
-
-
-  try{
-
+    try {
       database.onValue.listen((event) {
         print('realtime data');
         carParsedrealtime.clear();
         print('cleared');
-for (var childsnapshot in event.snapshot.children) {
-  print('looping realitme');
-  String tettes =  json.encode(childsnapshot.value);
-  Map<String,dynamic> decodedrealtime = json.decode(tettes);
-  print(decodedrealtime);
-  carParsedrealtime.add(decodedrealtime);
-  print(carParsedrealtime);
+        for (var childsnapshot in event.snapshot.children) {
+          print('looping realitme');
+          String tettes = json.encode(childsnapshot.value);
+          Map<String, dynamic> decodedrealtime = json.decode(tettes);
+          print(decodedrealtime);
+          carParsedrealtime.add(decodedrealtime);
+          print(carParsedrealtime);
+        }
 
-}
-
-  print('this is the list ${carParsedrealtime}');
+        print('this is the list ${carParsedrealtime}');
 
 // print('entered2');
 //               print(event.snapshot.value);
@@ -152,26 +140,21 @@ for (var childsnapshot in event.snapshot.children) {
 // final childKey = event.snapshot.key;
 // print(childKey);
 
-//         carParsed ={childKey!:event.snapshot.value}; 
-        print(carParsedrealtime);    
-        
-         print('leaved3');
-               print('emiting new state agaon00');
-  emit(VehiclesGetSuccess());
+//         carParsed ={childKey!:event.snapshot.value};
+        print(carParsedrealtime);
 
-
+        print('leaved3');
+        print('emiting new state agaon00');
+        emit(VehiclesGetSuccess());
       });
-
-
-}catch(e){
-emit(VehiclesGetFailure());
-
-}
-}
+    } catch (e) {
+      emit(VehiclesGetFailure());
+    }
+  }
 
   @override
   void onChange(Change<VehiclesState> change) {
     super.onChange(change);
-print('this is the change dd $change');
+    print('this is the change dd $change');
   }
 }
